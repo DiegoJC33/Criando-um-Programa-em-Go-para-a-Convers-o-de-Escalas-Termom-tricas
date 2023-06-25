2b9d02dff937e5e786d4a6a35f692eaa1e32f66b
Diego Carvalho <diego.jcarvalho25@gmail.com>
2023-06-25 19:13:17 -0300

    Desafio

---
+++ c-kelvin.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+const ebulicaoK = 373
+
+func main() {
+
+	tempC := (ebulicaoK - 273)
+
+	fmt.Println("O ponto de ebulicação da água em graus C é = ", tempC)
+
+}
